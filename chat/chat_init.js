f45ec74399be55af5700cbb8ee800ba45ad72a74
Mathieu Lecarme <mathieu@garambrogne.net>
2010-02-07 19:37:13 +0100

    ask for direct talk, with response

--- chat/chat_init.js
+++ chat/chat_init.js
@@ -1,29 +1,62 @@
 /**
  * Invite someone to talk
  */
-poem.Tchat.prototype.wannaTalk = function(to) {
+poem.Tchat.prototype.wannaTalk = function(to, callback, error) {
+	/*
 	this.connection.send(
 		$msg({type:'headline', to:to})
 			.c('wannatalk',{})
 			.tree()
-	);	
+	);*/
+	this.connection.sendIQ($iq({to:to})
+			.c('wannatalk',{type:"question"})
+			.tree(),
+		function(stanza) {//callback
+			poem.log(['wanna yes!', stanza]);
+			callback(stanza);
+		},
+		function(stanza) {//error
+			error(stanza);
+		},
+		30000);
 };
 poem.Tchat.prototype.handleWannaTalk = function(handler) {
-	this.handleHeadline(function(head){
+	/*this.handleHeadline(function(head){
 		if(head.getElementsByTagName('wannatalk').length > 0) {
 			poem.log('wanna talk?');
 			handler(head);
 		}
 	}.bind(this));
 	//[FIXME] le bind n'est pas bon
+	*/
+	this.handleIQ(function(iq) {
+		if(iq.getElementsByTagName('wannatalk').length > 0) {
+			poem.log(['wanna iq', iq, this]);
+			var id = iq.getAttribute('id');
+			var from = iq.getAttribute('from');
+			var wannatalk = iq.getElementsByTagName('wannatalk')[0];
+			var type = wannatalk.getAttribute('type');
+			if(type == "question") {
+				var answer = handler(from, wannatalk);
+				this.connection.send($iq({id:id, type:'result', to:from})
+					.c('wannatalk', {type:"answer"})
+					.t(answer[0] ? '1':'0')
+					.tree());
+				if(answer.length > 1 && answer[0]) {
+					this.flush();
+					answer[1]();
+				}
+			}
+		}
+	});
 };
 
 poem.behaviors.append(function() {
-	xmpp.handleWannaTalk(function(head){
-		//poem.log(this);
-		var name = head.getAttribute('from').split('@')[0];
-		if(window.confirm(name + ' wonts to talk with you')) {
+	xmpp.handleWannaTalk(function(who){
+		var name = who.split('@')[0];
+		var answer = window.confirm(name + ' wonts to talk with you');
+		return [answer, function() {
 			document.location.href = Drupal.settings.strophe.direct_talk_url + name;
-		}
+		}];
 	});
 });
--- muc/muc_presence.js
+++ muc/muc_presence.js
@@ -1,9 +1,20 @@
 poem.behaviors.append(function(){
     var directTalk = function(who) {
         poem.log('id', Drupal.settings.strophe.direct_talk_url + who);
-        xmpp.wannaTalk(who + '@' + Drupal.settings.xmpp.jid.split('@')[1]);
-        xmpp.flush();
-        document.location.href = Drupal.settings.strophe.direct_talk_url + who;
+        xmpp.wannaTalk(who + '@' + Drupal.settings.xmpp.jid.split('@')[1],
+            function(stanza){//callback
+                poem.log(['response', stanza, $(stanza, 'wannatalk').text()]);
+                if($(stanza, 'wannatalk').text() == '1') {
+                    document.location.href = Drupal.settings.strophe.direct_talk_url + who;
+                 } else {
+                    alert(who + " doesn't wont to talk with you");
+                 }
+            },
+            function(stanza){//error
+                alert(who + " is too slow, or dead");
+            });
+        //xmpp.flush();
+        //document.location.href = Drupal.settings.strophe.direct_talk_url + who;
     };
     var presence = $('#xmpp_presence');
     muc_room.handleAvailable(function(pres){
